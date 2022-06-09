a6945c8be225baa2e9f6f5bf4978ee40c4a8a05b
Arthur Bartoli <77788177+ArthurBartoli@users.noreply.github.com>
2022-06-09 22:56:58 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 from obj.questlog import QuestLog
 
-liste = QuestLog('le kk')
+liste = QuestLog('hello')
 
 liste.add_objective('Acheter des bananes')
-liste.add_objective('KK')
+liste.add_objective('hello')
 
 liste.check_objective(0)
 
